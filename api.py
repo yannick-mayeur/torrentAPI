2ebc85f2c34f6b6e1888e60b682e58c20dcb2a2a
Yannick Mayeur <yannick.mayeur@gmail.com>
2018-10-09 16:50:46 +0200

    Add torrent post

---
+++ api.py
@@ -0,0 +1,54 @@
+from flask import Flask
+from flask_restful import Resource, Api, reqparse
+import werkzeug, os
+
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = set(['torrent'])
+
+app = Flask(__name__)
+api = Api(app)
+parser = reqparse.RequestParser()
+parser.add_argument('file',type=werkzeug.datastructures.FileStorage, location='files')
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+
+class Torrent(Resource):
+
+    def post(self):
+        data = parser.parse_args()
+        if data['file'] == "":
+            return {
+                    'data':'',
+                    'message':'No file found',
+                    'status':'error'
+                    }
+        torrent = data['file']
+
+        if torrent and allowed_file(torrent.filename):
+            filename = torrent.filename
+            torrent.save(os.path.join(UPLOAD_FOLDER,filename))
+            return {
+                    'data':'',
+                    'message':'torrent uploaded',
+                    'status':'success'
+                    }
+        return {
+                'data':'',
+                'message':'Something when wrong',
+                'status':'error'
+                }
+
+
+api.add_resource(HelloWorld, '/')
+api.add_resource(Torrent,'/upload')
+
+if __name__ == '__main__':
+    app.run('0.0.0.0')
